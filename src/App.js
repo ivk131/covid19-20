b87dc1b38b09522af67232e33a423de33cb1a741
Vikash Kumar <vikashsmart1996@gmail.com>
2020-05-12 13:41:30 +0530

    covid19 tracker website

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
+import styles from "./App.css";
+
+class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <h1 align='center'>Covid-19</h1>
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Chart } from './Chart/chart';
+export { default as CountryPicker } from './CountryPicker/countrypicker';
+export { default as Cards } from './Card/card';
+
